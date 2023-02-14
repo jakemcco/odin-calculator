eec9198038e20dcdf1d322b9a5c7191febdb429a
Jake McCormick <122489471+jakemcco@users.noreply.github.com>
2023-02-14 12:47:57 -0500

    Create initial outline of function blocks

---
+++ calculator.js
@@ -0,0 +1,85 @@
+
+//Inputs: UI buttons, keyboard input
+function inputHandler() {
+    //User input should be a series of numbers delimited by operator symbols, probably a string
+
+    //Could use Array to store each item as a number object or operator object with value equal to the number/operator name
+    let userSubmission = [
+        {
+            number:value,
+            operator:value
+        },
+        {
+            number:value,
+            operator:value
+        },
+        {
+            number:value,
+            operator:value
+        },
+    ];
+
+}
+
+//Outputs: Text display
+function updateDisplay() {
+
+}
+//User function calls: 
+
+function undo() {
+
+}
+
+function clear() {
+
+}
+
+function calculate() { //Equals button calls this
+
+}
+
+
+
+
+//Internals
+function validateInput(userInput) {
+
+}
+
+function parseInput(cleanedInput) {
+
+}
+
+function operate(a, b, operator) {
+
+}
+
+
+//Math Operations
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+
+
+
+
+
+
+
+
+
+
